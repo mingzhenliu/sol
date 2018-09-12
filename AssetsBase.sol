47b3ab2c189031aad62b3a10bf6c95e90c5be08b
liuqikri <1908579209@qq.com>
2018-09-12 17:36:49 +0800

    校验和event

--- AssetsBase.sol
+++ AssetsBase.sol
@@ -9,17 +9,23 @@ contract AssetsBase{
 	using LibString for *;
     address accountContractAddress;
     uint nonce;
+
+    event assetsBaseEvent(address accountContractAddress, uint nonce);
+
     function AssetsBase(address accountContractAddress1) public {
         accountContractAddress = accountContractAddress1;
         nonce = 0;
+        assetsBaseEvent(accountContractAddress1,nonce);
     }
 
+    //方法中传入签名,需要通过签名门限校验
     function check(address[] signers, string nonce1) public returns(bool) {
 		address[] memory hashSigners =  new address[](signers.length);
 		for(uint i=0; i< signers.length; ++i)
          {
              hashSigners[i]=signers[i];
          }
+        //校验发起方的签名门限和nonce值，若通过才会进行减去余额操作 
         if(accountABI(accountContractAddress).checkThreshold(hashSigners))
         {
             if(nonce == uint(nonce1.toInt()))
